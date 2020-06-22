de9b941799ff817e17b7bcfc5b01c2d4c7f471b1
uznzafer <uznzafer@hotmail.com>
2020-06-22 16:20:17 +0300

    Add project files.

---
+++ Mulakat Takip/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mulakat_Takip.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
